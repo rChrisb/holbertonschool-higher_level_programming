a2801fd59137559453153f54f80b7f0e9372dc66
rChrisb <5767@holbertonstudents.com>
2023-01-24 09:46:38 +0100

    Write a function that prints x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    times = 0
+    try:
+        while times < x:
+            if times == x - 1:
+                print("{}".format(my_list[times]))
+            else:
+                print("{}".format(my_list[times]), end="")
+            times += 1
+    except IndexError:
+        print()
+        return times
+
+    return times
