91d5e2a784edae659a817a33af745cc861b52f4c
rChrisb <5767@holbertonstudents.com>
2023-01-30 10:56:34 +0100

    class with private instance attributes and properties, and with public instance method

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Area of a square"""
+
+
+class Square:
+    """class Square that defines a square
+    by Private instance attribute: size
+    with Instantiation with optional size
+    and a Public instance method: def area(self)
+    that returns the current square area """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
