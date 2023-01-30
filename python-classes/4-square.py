91649ef56f8b59c5e52b55f2c3114ff36045d838
rChrisb <5767@holbertonstudents.com>
2023-01-30 11:04:38 +0100

    class with private instance attributes and properties, and with public instance method

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Area of a square"""
+""" Access and update private attribute"""
 
 
 class Square:
@@ -23,6 +23,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
