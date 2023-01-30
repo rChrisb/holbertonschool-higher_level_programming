13c7aa021b08e9b9e260319f8f84c33f06bc26e4
rChrisb <5767@holbertonstudents.com>
2023-01-30 18:33:02 +0100

    Write a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Say my name"""
+
 
 def say_my_name(first_name, last_name=""):
+    """function that prints 'My name
+    is <first name> <last name>'"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
