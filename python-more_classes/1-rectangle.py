024c9caa4821213e57300d7efc232e9d8721801d
rChrisb <5767@holbertonstudents.com>
2023-01-31 12:53:13 +0100

    based on previous task, add private instances, getters and setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,6 +23,10 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -31,4 +35,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
