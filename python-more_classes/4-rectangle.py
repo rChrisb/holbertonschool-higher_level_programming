c94c63dddf6a5b49065e856f0d452a33a4511f1d
rChrisb <5767@holbertonstudents.com>
2023-01-31 13:31:13 +0100

    based on previous task, implement repr method

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Eval is magic"""
+
+
+class Rectangle:
+    """class Rectangle that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        shape = ""
+        if self.__width == 0 or self.__height == 0:
+            return shape
+        for h in range(self.__height):
+            if h == self.__height - 1:
+                shape += "#"*self.__width
+            else:
+                shape += "#"*self.__width + "\n"
+        return shape
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
