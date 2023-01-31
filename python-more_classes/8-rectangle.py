ad38617be2a5d2bae866eae82d419375af0284ae
rChrisb <5767@holbertonstudents.com>
2023-01-31 15:07:08 +0100

    based on previous task, implement a new static method returning the rectangle with the bigger area" "

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+"""How many instances"""
+
+
+class Rectangle:
+    """class Rectangle that defines a rectangle"""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        shape = ""
+        if self.__width == 0 or self.__height == 0:
+            return shape
+        if type(self.print_symbol) is int:
+            stringed = str(self.print_symbol)
+            for h in range(self.__height):
+                if h == self.__height - 1:
+                    shape += stringed*self.__width
+                else:
+                    shape += stringed*self.__width + "\n"
+            return shape
+        if type(self.print_symbol) is list:
+            stringed = str(self.print_symbol)
+            for h in range(self.__height):
+                if h == self.__height - 1:
+                    shape += stringed*self.__width
+                else:
+                    shape += stringed*self.__width + "\n"
+            return shape
+
+        for h in range(self.__height):
+            if h == self.__height - 1:
+                shape += self.print_symbol*self.__width
+            else:
+                shape += self.print_symbol*self.__width + "\n"
+        return shape
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        else:
+            return rect_1
