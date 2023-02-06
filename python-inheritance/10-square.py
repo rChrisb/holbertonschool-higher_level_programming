637f1e65e93c9b9cc41e3b824d62cd1786ae0b17
rChrisb <5767@holbertonstudents.com>
2023-02-06 12:47:20 +0100

    class square child of class rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Square #1"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    def __intit__(self, size):
+        super().__init__(self.__size, self.__size)
+        self.__size = size
+    #     self.__size = size
+    #     self.integer_validator("size", size)
+
+    # def area():
+    #     return self.size ** 2
+
+    # def __str__(self):
+    #     return f"[Rectangle] {size}/{size}"
