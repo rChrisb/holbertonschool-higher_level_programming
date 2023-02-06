3e6ab382080da34cab0d85c2d1a357d31ff1d04c
rChrisb <5767@holbertonstudents.com>
2023-02-06 08:39:58 +0100

    Write a function def copy_list(a_list): that returns a copy of a list

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""Copy a list object"""
-
-
 def copy_list(a_list):
-    """ function that returns a copy of a list"""
-
-    copy = a_list.copy()
-    return copy
+    return a_list.copy()
