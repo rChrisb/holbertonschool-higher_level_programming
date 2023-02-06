55e04b323a70742992f089af19b18c090dfa7f2f
rChrisb <5767@holbertonstudents.com>
2023-02-06 18:36:25 +0100

    based on previous task, add a Public method def reload_from_json(self, json): that replaces all attributes of the Student instance

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -20,6 +20,6 @@ class Student:
             self.last_name = json.get("last_name")
             self.age = json.get("age")
         else:
-            self.first_name = first_name
-            self.last_name = last_name
-            self.age = age
+            self.first_name = self.first_name
+            self.last_name = self.last_name
+            self.age = self.age
