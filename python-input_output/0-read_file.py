5ff0d33e1f2e367545a952cf9f806a2f10d84010
rChrisb <5767@holbertonstudents.com>
2023-02-06 14:24:43 +0100

    functions that reads a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, "r") as file:
-        print(file.read())
+        print(file.read(), end="")
