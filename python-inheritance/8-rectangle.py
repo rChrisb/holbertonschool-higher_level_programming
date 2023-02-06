0baa9394115ae84dd514ddf00aa3844951f4f25b
rChrisb <5767@holbertonstudents.com>
2023-02-06 11:42:25 +0100

    Write a class Rectangle that inherits from BaseGeometry, with private variables height and width that must be validated by 'integer_validator' function

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", self.__height)
+        self.integer_validator("width", self.__width)
