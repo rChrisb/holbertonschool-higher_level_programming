e58c232e0caef3d8c0d1da8a1bd65513c9bbd154
rChrisb <5767@holbertonstudents.com>
2023-02-06 16:00:15 +0100

    writes an Object to a text file, using a JSON representation

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,8 +6,8 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
-    if type(my_obj) is set:
-        raise PermissionError("13", f"Permission denied: '{filename}'")
+    # if type(my_obj) is set:
+    #     raise PermissionError("13", f"Permission denied: '{filename}'")
     json_string = json.dumps(my_obj)
     with open(filename, "a") as file:
         file.write(json_string)
