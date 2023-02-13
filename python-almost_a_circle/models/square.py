28026ea5daa5e526d1c955cfc1eda92c88ae820d
rChrisb <5767@holbertonstudents.com>
2023-02-13 13:04:55 +0100

    square class, inheriting from rectangle, with getter and setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,6 +10,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+        self.integer_validator("width", value)
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} -\
  {self.height}"
