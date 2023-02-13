881943fd36ced7e6858e39273253802d0704b022
rChrisb <5767@holbertonstudents.com>
2023-02-13 12:12:37 +0100

    class Rectangle that inherits from Base, with setters, getters, area, display, str and update methods

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,6 +77,20 @@ class Rectangle(Base):
         for w in range(self.__height):
             print("#" * self.__width)
 
+    def update(self, *args):
+        """ assigns an argument to each attribute"""
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
+        if len(args) >= 1:
+            self.id = args[0]
+
     def __str__(self):
+        """str method"""
         return f"""[Rectangle] ({self.id}) {self.__x}/{self.__y} \
 - {self.__width}/{self.__height}"""
