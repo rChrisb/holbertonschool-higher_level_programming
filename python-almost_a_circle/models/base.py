a381bdeb0e5b32f1fac4b23c9e79050192b9c74f
rChrisb <5767@holbertonstudents.com>
2023-02-13 15:08:41 +0100

    returns the list of the JSON string representation json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -39,6 +39,7 @@ class Base:
                     json_file.write(content_json)
 
     def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
