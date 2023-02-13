671acbfa6ab9b11bf2d125fb90c269ea72b628b7
rChrisb <5767@holbertonstudents.com>
2023-02-13 10:04:33 +0100

    class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,39 +9,39 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
-        self.width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def width(self, value):
-        self.height = value
+        self.__height = value
 
     @property
     def x(self):
-        return self.x
+        return self.__x
 
     @x.setter
     def width(self, value):
-        self.x = value
+        self.__x = value
 
     @property
     def y(self):
-        return self.y
+        return self.__y
 
     @y.setter
     def width(self, value):
-        self.y = value
+        self.__y = value
