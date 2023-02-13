f72b03c884382fdc1eaf15fa6d5d17452a92506e
rChrisb <5767@holbertonstudents.com>
2023-02-13 12:03:30 +0100

    tests for Base Id

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -19,6 +19,8 @@ class TestsBaseId(unittest.TestCase):
         self.assertEqual(Base().id, 1)
         self.assertEqual(Base().id, 2)
         self.assertEqual(Base(89).id, 89)
+        self.assertNotEqual(Base().id, 90)
+        self.assertEqual(Base().id, 4)
 
 
 if __name__ == '__main__':
