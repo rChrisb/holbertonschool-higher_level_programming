ebea4d534a1e68b47b4829c260e9d4dc58d938cf
rChrisb <5767@holbertonstudents.com>
2023-02-13 17:23:28 +0100

    Add 2 tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -5,7 +5,7 @@ import unittest
 from models.base import Base
 
 
-class TestsBaseId(unittest.TestCase):
+class TestsBase(unittest.TestCase):
     def test_base(self):
         # first_base = Base()
         # third_base = Base(89)
@@ -22,10 +22,10 @@ class TestsBaseId(unittest.TestCase):
         self.assertNotEqual(Base().id, 90)
         self.assertEqual(Base().id, 4)
 
-    # def test_id_incremental(self):
-    #     self.assertEqual(Base().id, 5)
-    #     self.assertEqual(Base().id, 6)
+    def test_id_incremental(self):
+        self.assertEqual(Base().id, 5)
+        self.assertEqual(Base().id, 6)
 
-    # def test_not_none(self):
-    #     self.assertNotEqual(Base().id, None)
+    def test_not_none(self):
+        self.assertNotEqual(Base().id, None)
 
