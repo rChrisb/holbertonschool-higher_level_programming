c25b26c9e2c9fc53e1b86d8eb2deed4d230dad55
rChrisb <5767@holbertonstudents.com>
2023-02-13 08:51:49 +0100

    Testing base id, (task 1)

---
+++ python-almost_a_circle/tests/test_baseid.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import unittest
+
+from models.base import Base
+
+
+class TestsBaseId(unittest.TestCase):
+    def test_base(self):
+        # first_base = Base()
+        # third_base = Base(89)
+        # second_base = Base()
+        # fourth_base = Base()
+
+        # self.assertEqual(first_base.id, 1)
+        # self.assertEqual(second_base.id, 2)
+        # self.assertEqual(third_base.id, 89)
+        # self.assertEqual(fourth_base.id, 3)
+        self.assertEqual(Base().id, 1)
+        self.assertEqual(Base().id, 2)
+        self.assertEqual(Base(89).id, 89)
+
+
+if __name__ == '__main__':
+    unittest.main()
