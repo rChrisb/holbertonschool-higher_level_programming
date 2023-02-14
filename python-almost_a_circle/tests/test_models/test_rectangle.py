b8e91da27911d319d9566845c3a0a5fe55551fb0
rChrisb <5767@holbertonstudents.com>
2023-02-14 10:22:01 +0100

    tests for display method

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -36,7 +36,9 @@ class TestsRectangle(unittest.TestCase):
         self.assertEqual(Rectangle(2, 5).area(), 10)
     
     def test_str(self):
-        self.assertEqual(Rectangle(2, 4).__str__(), "[Rectangle] (21) 0/0 - 2/4")
+        self.assertEqual(Rectangle(2, 4).__str__(), "[Rectangle] (23) 0/0 - 2/4")
     
     def test_display(self):
         self.assertEqual(Rectangle(1, 1, 1, 1).display(), None)
+        self.assertEqual(Rectangle(1, 1, 1).display(), None)
+        self.assertEqual(Rectangle(1, 1).display(), None)
