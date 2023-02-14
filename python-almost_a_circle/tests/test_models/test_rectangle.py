a73348f2f317099f4cd01851824fa31fe783edb5
rChrisb <5767@holbertonstudents.com>
2023-02-14 09:47:11 +0100

    tests for Rectangle integer validator

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,5 +6,10 @@ from models.rectangle import Rectangle
 
 
 class TestsRectangle(unittest.TestCase):
-    def test_instanciation(self):
+    def test_instantiation(self):
         self.assertEqual(Rectangle(1, 2).width, 1)
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2).width
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2").width
+        
