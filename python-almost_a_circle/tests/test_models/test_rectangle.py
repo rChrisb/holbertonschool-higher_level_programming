54afa5469d935cc4862f54cd0817122ffc178612
rChrisb <5767@holbertonstudents.com>
2023-02-14 09:33:48 +0100

    tests for Rectangle __init__

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
 import unittest
+
+from models.rectangle import Rectangle
+
+
+class TestsRectangle(unittest.TestCase):
+    def test_instanciation(self):
+        self.assertEqual(Rectangle(1, 2).width, 1)
