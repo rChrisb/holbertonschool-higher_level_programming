2149007cea561ca18782c5c6faa6a86e06e53894
rChrisb <5767@holbertonstudents.com>
2023-03-13 16:58:38 +0100

    Cities of California

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of Califormnia
 
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California");
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
