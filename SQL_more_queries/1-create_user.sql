3d17cea91ecce17ca3349a294da14443a0214e31
rChrisb <5767@holbertonstudents.com>
2023-03-13 15:06:57 +0100

    Write a script that creates the MySQL server user user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Root user
 CREATE USER if not exists user_0d_1@localhost IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
