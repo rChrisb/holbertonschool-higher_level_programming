f991b0cbaf69a058c79c2fa002716cc1efbbbdc7
rChrisb <5767@holbertonstudents.com>
2023-03-14 12:14:19 +0100

    list states beginning with the letter N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Filter states"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+        )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from states WHERE name LIKE 'N%'")
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
