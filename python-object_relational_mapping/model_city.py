84887c69d7178d6369ae66c0cdfc6370dc062fa0
rChrisb <5767@holbertonstudents.com>
2023-03-14 17:45:17 +0100

    create new city class for the city table

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""First state model"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+import MySQLdb
+
+Base = declarative_base()
+
+
+class City(Base):
+    """links to the MySQL table states"""
+    __tablename__ = "cities"
+
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user="root",
+        password="root"
+        )
