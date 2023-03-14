f09289a3caabfff937dbf17fdd6ff2fd33419f2e
rChrisb <5767@holbertonstudents.com>
2023-03-14 13:52:28 +0100

    Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Filter  by user input"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+        )
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name \
+        from cities join states on cities.state_id = states.id"
+        )
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
