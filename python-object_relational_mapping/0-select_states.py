59717a1e88642a2426e8beae58e8f5efa020f06e
rChrisb <5767@holbertonstudents.com>
2023-03-14 12:01:32 +0100

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user='root',
-        password='root',
-        database='hbtn_0e_0_usa'
+        user=username,
+        password=password,
+        database=database
         )
     cursor = connection.cursor()
     cursor.execute("SELECT * from states ORDER BY id ASC")
