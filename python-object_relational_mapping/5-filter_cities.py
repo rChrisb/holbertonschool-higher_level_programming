c2a2f4065ce2daefdce6bd834be5221384d96369
rChrisb <5767@holbertonstudents.com>
2023-03-14 14:22:44 +0100

    All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Filter  by user input"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+        )
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT cities.name \
+        from cities join states on cities.state_id = states.id\
+            WHERE BINARY states.name = %s ORDER BY cities.id", (state_name,)
+        )
+
+    results = cursor.fetchall()
+    for row in range(len(results)):
+        if row == len(results) - 1:
+            for (city) in results[row]:
+                print(city)
+        else:
+            for (city) in results[row]:
+                print(city, end=", ")
+
+    cursor.close()
+    connection.close()
