dcb96d34ce4942cae8103e318dbd43a21767c9d8
rChrisb <5767@holbertonstudents.com>
2023-03-14 12:26:03 +0100

    User enters a the state as a fourth argument to display it

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Filter  by user input"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        password=password,
+        database=database
+        )
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * from states WHERE name = '{sys.argv[4]}'")
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
