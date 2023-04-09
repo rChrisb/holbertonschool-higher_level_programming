0bf27b9ce2b75cc38ad3d9e365416e90236f5404
rChrisb <5767@holbertonstudents.com>
2023-04-09 15:58:04 +0200

    Write a script that prints x times “C is fun”

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let numberOfTimes = process.argv[2];
+
+if (process.argv[2] < 0) {
+  numberOfTimes = 0;
+  process.argv[2] *= -1;
+}
+if (!Number.isInteger(+process.argv[2]))
+  console.log("Missing number of occurrences");
+else {
+  for (let times = 0; times < numberOfTimes; times++) {
+    console.log("C is fun");
+  }
+}
