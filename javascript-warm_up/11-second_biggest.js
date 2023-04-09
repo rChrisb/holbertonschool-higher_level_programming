fd8d2976ad879303494421701ff6de463aaf2935
rChrisb <5767@holbertonstudents.com>
2023-04-09 17:03:03 +0200

    Write a script that searches the second biggest integer in the list of arguments.

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 
-if (!process.argv[2]) console.log(0);
-if (process.argv.length === 3) console.log(0);
+if (process.argv.length <= 3) console.log(0);
+else {
+  let max = 0;
+  let secondMax = 0;
+  const newList = [];
 
-let max = 0;
-let secondMax = 0;
-const newList = [];
+  for (let i = 0; i < process.argv.length - 2; i++) {
+    if (max < +process.argv[i]) max = +process.argv[i];
+  }
 
-for (let i = 0; i < process.argv.length - 2; i++) {
-  if (max < process.argv[i]) max = process.argv[i];
-}
-for (let i = 0; i < (process.argv).slice(2).length; i++) {
-  if (process.argv[i] !== max) newList.push(process.argv[i]);
-}
+  for (let i = 0; i < process.argv.slice(2).length; i++) {
+    if (+process.argv.slice(2)[i] !== max) { newList.push(+process.argv.slice(2)[i]); }
+  }
 
-for (let i = 0; i < newList.length - 2; i++) {
-  if (secondMax < newList[i]) secondMax = newList[i];
+  for (let i = 0; i < newList.length; i++) {
+    if (secondMax < +newList[i]) secondMax = +newList[i];
+  }
+  console.log(secondMax);
 }
-console.log(secondMax);
