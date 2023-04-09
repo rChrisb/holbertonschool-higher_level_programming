7110e9395f2fe78de20540ea76029267cbdc44f8
rChrisb <5767@holbertonstudents.com>
2023-04-09 14:55:02 +0200

    Write a script that prints a message depending of the number of arguments passed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 let numberOfArgs = 0;
 
 process.argv.forEach(element => {
-	numberOfArgs++;
+  numberOfArgs++;
 });
 
 if (numberOfArgs < 3) console.log('No argument');
-if (numberOfArgs == 3) console.log('Argument found');
+if (numberOfArgs === 3) console.log('Argument found');
 if (numberOfArgs > 3) console.log('Arguments found');
