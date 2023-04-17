2bfcd0c5d31166abd0d80d254c9fd4f2abf8f009
rChrisb <5767@holbertonstudents.com>
2023-04-17 03:18:20 +0200

    Write a script that computes the number of tasks completed by user id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 const tasksMap = {};
 request(process.argv[2], (error, response, jsonContent) => {
   if (error) throw error;
-  content = JSON.parse(jsonContent);
+  const content = JSON.parse(jsonContent);
   const tasksCompleted = [];
   content.forEach((task) => {
     if (task.completed) tasksCompleted.push(task);
